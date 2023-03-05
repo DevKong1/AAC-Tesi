9cfc76698ec15400b7e902c23bdeb4bf31d80ad8
Konrad Gomulka <konrad.gomulka@studio.unibo.it>
2023-03-05 19:56:19 +0100

    WIP Categories page

--- apps/expo/app/views/pictogramSelection.tsx
+++ apps/expo/app/views/pictogramSelection.tsx
@@ -2,18 +2,55 @@ import React from "react";
 import {
   ActivityIndicator,
   FlatList,
+  Image,
   Text,
   TextInput,
+  TouchableOpacity,
   View,
+  type ImageSourcePropType,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useQuery } from "@tanstack/react-query";
 
 import { type Pictogram } from "../../src/types/commonTypes";
-import { usePictoramStore } from "../store/store";
 
-const PictogramSelection = () => {
-  const { data: pictograms, isLoading } = useQuery(["pictograms"], async () => {
+const CategoriesView: React.FC<{
+  categories: string[][];
+  setCategory: React.Dispatch<React.SetStateAction<string>>;
+}> = ({ categories }) => {
+  return (
+    <View className="h-full w-full">
+      <FlatList
+        numColumns={2}
+        data={categories}
+        renderItem={({ item }) => {
+          return (
+            <View className="mx-auto my-6 w-2/5 rounded-lg  bg-white/10 p-4">
+              <TouchableOpacity className="items-center justify-center">
+                <View className="w-full">
+                  <Image
+                    className="h-12 object-contain"
+                    source={item[1] as ImageSourcePropType}
+                    alt={`Image of ${item[0] as string}`}
+                  />
+                </View>
+                <Text className="color-white"> {item[0] as string}</Text>
+              </TouchableOpacity>
+            </View>
+          );
+        }}
+        keyExtractor={(_, index) => index.toString()}
+      />
+    </View>
+  );
+};
+
+const PictogramsView: React.FC = () => {
+  const {
+    data: pictograms,
+    error,
+    isLoading,
+  } = useQuery(["pictograms"], async () => {
     console.log("Loading pictorams...");
     return (await import("../../assets/dictionaries/Dizionario_en.json"))
       .default;
@@ -27,6 +64,34 @@ const PictogramSelection = () => {
       </View>
     );
 
+  if (error)
+    return (
+      <View className="h-3/4 w-full items-center justify-center">
+        <Text className="py-4 text-xs text-white">
+          Error loading data, try restarting the app!
+        </Text>
+      </View>
+    );
+
+  return <View></View>;
+};
+
+const PictogramSelection = () => {
+  const categories = [
+    ["basic concepts", require("../../assets/pictograms/img_9202.png")],
+    ["preposition", require("../../assets/pictograms/img_11709.png")],
+    ["verb", require("../../assets/pictograms/img_11741.png")],
+    ["animal", require("../../assets/pictograms/img_6901.png")],
+    ["feeling", require("../../assets/pictograms/img_7251.png")],
+    ["color", require("../../assets/pictograms/img_34483.png")],
+    [
+      "core vocabulary-communication",
+      require("../../assets/pictograms/img_34826.png"),
+    ],
+  ];
+  const [selectedCategory, setCategory] = React.useState("");
+  const [searchText, setText] = React.useState("");
+
   return (
     <SafeAreaView className="h-full w-full flex-col items-center rounded">
       <View className="w-full px-4 py-8">
@@ -35,18 +100,7 @@ const PictogramSelection = () => {
           placeholder="Search for a pictogram..."
         />
       </View>
-      <FlatList
-        className="w-full"
-        data={[
-          ...new Set(
-            (pictograms as Pictogram[]).flatMap((el) => el.categories),
-          ),
-        ]}
-        renderItem={({ item }) => {
-          return <Text>{item}</Text>;
-        }}
-        keyExtractor={(item) => item}
-      />
+      <CategoriesView categories={categories} setCategory={setCategory} />
     </SafeAreaView>
   );
 };
